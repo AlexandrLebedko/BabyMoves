61ae50ee24f4934506cc73b5d267035acfaeba25
Alexander Lebedko <alebedko@retechlabs.com>
2019-07-09 14:10:59 +0700

    Initial commit

---
+++ BabyMoves/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BabyMoves
+//
+//  Created by Alexandr Lebedko on 09/07/2019.
+//  Copyright © 2019 Alexandr Lebedko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
